d5d975a9ebf13c7e8cb9a156a153186419c30cc8
luca-dgdev <luca.dgdev@gmail.com>
2019-03-16 12:03:27 +0100

    fix navigation

--- src/main/java/com/example/thymeleafdemo/controller/WebController.java
+++ src/main/java/com/example/thymeleafdemo/controller/WebController.java
@@ -25,4 +25,14 @@ public class WebController {
 	public String pagina1(Model model) {
 		return "pagina1";
 	}
+	
+	@GetMapping("/pagina2")
+	public String pagina2(Model model) {
+		return "pagina2";
+	}
+	
+	@GetMapping("/403")
+	public String notPermittedPage(Model model) {
+		return "403";
+	}
 }
