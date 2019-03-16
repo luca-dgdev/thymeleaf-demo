644306aca658143ea5ca4511287f6f069668fb3e
luca-dgdev <luca.dgdev@gmail.com>
2019-03-16 13:59:45 +0100

    logging

--- src/main/java/com/example/thymeleafdemo/ThymeleafDemoApplication.java
+++ src/main/java/com/example/thymeleafdemo/ThymeleafDemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.thymeleafdemo;
 
+import java.util.logging.Logger;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThymeleafDemoApplication {
+public class ThymeleafDemoApplication implements CommandLineRunner {
 
+	private static final Logger log = Logger.getLogger(ThymeleafDemoApplication.class.getName());
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("ThymeleafDemoApplication started");
+		
 	}
 
 }
--- src/main/java/com/example/thymeleafdemo/config/GoogleAccountsAuthenticationEntryPoint.java
+++ src/main/java/com/example/thymeleafdemo/config/GoogleAccountsAuthenticationEntryPoint.java
@@ -1,6 +1,7 @@
 package com.example.thymeleafdemo.config;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -13,10 +14,13 @@ import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 
 public class GoogleAccountsAuthenticationEntryPoint implements AuthenticationEntryPoint {
+	
+	private static final Logger log = Logger.getLogger(GoogleAccountsAuthenticationEntryPoint.class.getName());
+	
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
+		log.info("commence");
 		UserService userService = UserServiceFactory.getUserService();
-
 		response.sendRedirect(userService.createLoginURL(request.getRequestURI()));
 	}
 }
